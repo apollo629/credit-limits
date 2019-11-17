f4a4c6a3263015a648dfcd38bd2b780386c9c315
Alperen İNAL <inal.alperen@gmail.com>
2019-11-17 13:00:25 +0300

    initial commit

---
+++ src/main/java/com/byinal/creditLimits/CreditLimitsApplication.java
@@ -0,0 +1,13 @@
+package com.byinal.creditLimits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditLimitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditLimitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byinal/creditLimits/CreditLimitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byinal.creditLimits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditLimitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
